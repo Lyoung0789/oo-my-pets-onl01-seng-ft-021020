05331a0ba9a93cb15cfa8e4a8529d42cec33935c
Lester Young <lyoung070789@gmail.com>
2020-02-24 15:49:09 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_reader :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+     @new_arr=[]
+     @@all << self
+  end 
+  
+  def species
+    @species = "human"
+  end 
+  
+  def say_species
+    return "I am a human."
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.count 
+    @@all.count
+  end 
+  
+  def self.reset_all 
+    @@all.clear
+  end 
+  
+  def cats
+    Cat.all.select {|i| i.owner.name == self.name}
+  end 
+  
+  def dogs 
+    Dog.all.select {|i| i.owner.name == self.name}
+  end 
 end
